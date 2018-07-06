f3d30e197a9f7ef1eb67ade7017a44ea23ec5eb8
Mateusz Ezechiel Górka <admin@yehezkel.cba.pl>
2018-07-06 15:04:34 +0200

    termreset and scanf

--- dex.c
+++ dex.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
@@ -11,9 +12,9 @@ struct _savexy {
 };
 
 static struct  termios default_term,
-               new_term;
-static int     has_new_term = 0;
-static void   *xy_stack = NULL;
+                       new_term;
+static int             has_new_term = 0;
+static void           *xy_stack = NULL;
 
 void termexit(void)
 {
@@ -23,19 +24,28 @@ void termexit(void)
   }
 }
 
-void terminit(void)
+int terminit(void)
 {
+  /* just resetting terminal to dexterm after 1 terminit call */
   if (has_new_term) {
     tcsetattr(0, TCSANOW, &new_term);
-    return;
+    return 1;
   }
 
   tcgetattr(0, &default_term);
   new_term = default_term;
   new_term.c_lflag &= ~(ICANON | ECHO);
   tcsetattr(0, TCSANOW, &new_term);
+  
   has_new_term = 1;
   atexit(termexit);
+  
+  return 1;
+}
+
+void termreset(void)
+{
+  printf("\ec");
 }
 
 void gotoxy(int x, int y)
@@ -43,33 +53,10 @@ void gotoxy(int x, int y)
   printf("\e[%u;%uH", y, x);
 }
 
-void savexy(void)
-{
-  struct _savexy *new;
-
-  new = malloc(sizeof(struct _savexy));
-  new->ptr = xy_stack;
-  new->x = term_x;
-  new->y = term_y;
-  xy_stack = new;
-}
-
-void loadxy(void)
-{
-  struct _savexy *data,
-                 *old;
-
-  data = xy_stack;
-  old = data->ptr;
-  gotoxy(data->x, data->y);
-  free(xy_stack);
-  xy_stack = old;
-}
-
 void getxy(int *x, int *y)
 {
   int ch,
-      rx
+      rx,
       ry;
 
   rx = 0;
@@ -86,6 +73,32 @@ void getxy(int *x, int *y)
 
   *x = rx;
   *y = ry;
+}
+
+void savexy(void)
+{
+  struct _savexy *new;
+  int             x,
+                  y;
+
+  new = malloc(sizeof(struct _savexy));
+  new->ptr = xy_stack;
+  getxy(&x, &y);
+  new->x = x;
+  new->y = y;
+  xy_stack = new;
+}
+
+void loadxy(void)
+{
+  struct _savexy *data,
+                 *old;
+
+  data = xy_stack;
+  old = data->ptr;
+  gotoxy(data->x, data->y);
+  free(xy_stack);
+  xy_stack = old;
 }
 
 int getch(void)
@@ -108,12 +121,29 @@ inline int kbhit(void)
   return r;
 }
 
+int __dexterm_scanf(const char *format, ...)
+{
+  struct termios tmp;
+  va_list        args;
+  int            r;
+
+  va_start(args, format);
+  tcgetattr(0, &tmp);
+  tcsetattr(0, TCSANOW, &default_term);
+  r = vscanf(format, args);
+  tcsetattr(0, TCSANOW, &tmp);
+  va_end(args);
+
+  return r;
+}
+
+#define scanf(__VA_LIST__) __dexterm_scanf(__VA_LIST__)
+
 int main()
 {
   int x, y;
 
   terminit();
-
   getxy(&x, &y);
   printf("%d;%d\n", x, y);
 
