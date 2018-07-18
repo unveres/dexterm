792fa349ea0f413daee558d10fc537d4884401b2
Mateusz Ezechiel Górka <admin@yehezkel.cba.pl>
2018-07-18 21:57:38 +0200

    acutal example, which doesn't work

--- dex.c
+++ dex.c
@@ -2,20 +2,27 @@
 #include <stdio.h>
 #include <dexterm.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 int main()
 {
-  /*int ch, t, tmp;*/
+  int x, y;  
 
   terminit();
-  
-  /* testing pipe */
-  while (kbwaiting())
-    printf("%c", getch());
+
+  ioctl(0, FIONREAD, &x);
+  printf("%d\n", kbwaiting());
+  printf("%d\n", x);
+
+  gotoxy(7,7);
+  getxy(&x, &y);
+
+  printf("::%d::%d::\n", x, y);
 
   
 
-  /*
+  /*int ch, t, tmp;
+
   for (;;) {
     for (t = 0; t < 250; ++t) {
       if (kbhit()) {
